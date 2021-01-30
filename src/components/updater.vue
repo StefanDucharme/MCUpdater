e56d029e363a3f95d378bdf44ff0a9de5cd7432e
Stefan <stdd108@gmail.com>
2021-01-29 22:07:20 -0600

    skel of updater

---
+++ src/components/updater.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="updater">
+    <h1>Updater</h1>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Updater",
+  components: {
+    
+  },
+  props: {
+    msg: String,
+    
+  },
+  methods: {
+    // function called for every file dropped or selected
+    
+  },
+  computed: {
+    
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
